7c0aa6453527dd9e65a476710f0fb5a8c54e52e2
Tanguy SAURA-NERI <2501@holbertonschool.com>
2020-09-16 10:50:21 +0000

    added a description to please betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int	main(void)
 {
 	char	*str;
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Is that what you want?
+ * Return: Always 0 (Success)
+ */
 int	main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
