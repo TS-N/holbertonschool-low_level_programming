50b18c41b9b3c9bfcf4dd30d3e794bbf683bbb5f
Tanguy SAURA-NERI <2501@holbertonschool.com>
2020-09-17 10:29:20 +0000

    1 - typo fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ int main(void)
 	else if (ld > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else
-		printf("Last digin of %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	return (0);
 }
