158ac092fef3f57a5611bd12a478413cc20618f4
Tanguy SAURA-NERI <2501@holbertonschool.com>
2020-09-17 11:30:34 +0000

    102 - add

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,34 +12,31 @@ int	main(void)
 	int c2;
 	int c3;
 	int c4;
-	int i;
-	int i2;
-	int i3;
 
 	c = '0';
-	i = -1;
 	while (c <= '9')
 	{
-		c2 = '0' + ++i;
-		i2 = -1;
-		while (c2 <= '8')
+		c2 = '0';
+		while (c2 <= '9')
 		{
-			c3 = '0' + i + ++i2;
-			i3 = -1;
+			c3 = '0';
 			while (c3 <= '9')
 			{
-				c4 = '1' + i + i2 + i3;
+				c4 = '0';
 				while (c4 <= '9')
 				{
-					putchar(c);
-					putchar(c2);
-					putchar(' ');
-					putchar(c3);
-					putchar(c4);
+					if ((c3 > c && c4 >= c2) || (c3 == c && c4 > c2))
+					{
+						putchar(c);
+						putchar(c2);
+						putchar(' ');
+						putchar(c3);
+						putchar(c4);
 					if (!(c == '9' && c2 == '8' && c3 == '9' && c4 == '9'))
 					{
 						putchar(',');
 						putchar(' ');
+					}
 					}
 					++c4;
 				}
