bb8c233e6dced8b23a8e4bf142bea0470ad3f324
TS-N <2501@holbertonschool.com>
2020-09-24 12:36:03 +0000

    trailing white space removed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,14 @@ int	main(void)
 	i = 1;
 	while (i <= 100)
 	{
+		if (i != 1)
+			printf(" ");
 		if (!(i % 3))
 			printf("Fizz");
 		if (!(i % 5))
 			printf("Buzz");
 		if (i % 3 && i % 5)
 			printf("%d", i);
-		printf(" ");
 		++i;
 	}
 	printf("\n");
