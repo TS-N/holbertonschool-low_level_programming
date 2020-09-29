a65f76208dde3e256ca2e28b7442e30f23982cc6
TS-N <2501@holbertonschool.com>
2020-09-29 10:01:41 +0000

    figuring out the checker

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,36 @@
 #include "holberton.h"
 
 /**
-* puts2 - print every other char of a string
-* @str: the string to manipulate
-*
-**/
+ * _strlen - gives the length of a string
+ * @s: the string from whitch len is found
+ *
+ * Return: the len of the string
+ **/
+int	_strlen(char *s)
+{
+	int	l = 0;
+
+	while (*s++)
+		++l;
+	return (l);
+}
+
+/**
+ * puts2 - print every other char of a string
+ * @str: the string to manipulate
+ *
+ **/
 void	puts2(char *str)
 {
-	while (str && *str)
+	int	l;
+	int	i;
+
+	l = _strlen(str);
+	i = 0;
+	while (i < l)
 	{
-		_putchar(*str);
-		if (str + 2)
-			str += 2;
+		_putchar(*(str + i));
+		i += 2;
 	}
 	_putchar('\n');
 }
