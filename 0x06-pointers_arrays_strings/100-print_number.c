28f96b7c2244567b4db9cf87746f7075f8da41ec
TS-N <2501@holbertonschool.com>
2020-10-01 15:19:45 +0000

    ex:100 create

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_number - like itoa
+ * @n: the number to print
+ *
+ **/
+void	print_number(int n)
+{
+	unsigned int	nb;
+	if (n < 0)
+	{
+		_putchar('-');
+		nb = -n;
+	}
+	else
+		nb = n;
+	if (nb >= 10)
+	{
+		print_number(nb / 10);
+		_putchar(nb % 10 + '0');
+		return;
+	}
+	_putchar(nb + '0');
+}
