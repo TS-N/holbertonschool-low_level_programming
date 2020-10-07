f65d90fd2e4e11d2cfa0979e0b38db1a3fa7fd4a
TS-N <2501@holbertonschool.com>
2020-10-07 17:21:08 +0000

    fix:100 edge case add

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -32,12 +32,12 @@ int	wildcmp(char *s1, char *s2)
 		if (*s2 == '*')
 			return (wildcmp(s1, s2 + 1));
 		else if (*s1 == *s2)
-			return (wildcmp(s1 + 1, s2 + 1));
+			return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2));
 		else if (*s1 != *s2 && *(s2 - 1) == '*')
 		{
 			if (!*s2)
 				return (1);
-			return (wildcmp(s1 + 1, s2));
+			return (*s1 ? wildcmp(s1 + 1, s2) : 0);
 		}
 		else
 			return (0);
