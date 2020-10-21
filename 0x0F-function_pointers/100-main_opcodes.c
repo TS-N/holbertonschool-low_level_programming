3ee10a1bd106b73030a8d35133bfc11120809e6b
TS-N <2501@holbertonschool.com>
2020-10-21 11:34:50 +0000

    ex:100 create + betty

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include "function_pointers.h"
+
+/**
+  * main -  program that prints the opcodes of its own main function
+  * @ac: the nb of args
+  * @av: the args
+  *
+  * Return: 0
+  **/
+int	main(int ac, char **av)
+{
+	int	nb;
+	int	i;
+	void	*f;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	nb = atoi(av[1]);
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	i = 0;
+	f = (*main);
+	while (i < nb)
+	{
+		if (i)
+			printf(" ");
+		printf("%x", *(char *)f & 0xff );
+		++f;
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
