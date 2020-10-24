1609b6778193e0359a7361fc5d05d61cf9a403dc
TS-N <2501@holbertonschool.com>
2020-10-24 20:19:25 +0000

    fix:3 for !format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -61,7 +61,7 @@ void print_all(const char * const format, ...)
 	va_start(valist, format);
 	i = 0;
 	bs = "";
-	while (format[i] && format)
+	while (format && format[i])
 	{
 		j = 0;
 		while (t_list[j].c)
