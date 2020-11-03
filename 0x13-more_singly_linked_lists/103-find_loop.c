cdb39aa1ac7cec98704a4e386cca639b4dbe91b2
TS-N <2501@holbertonschool.com>
2020-11-03 11:04:10 +0000

    add:103 create + betty

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop - finds the loop in a linked list using Floyd's algo
+  * @head: the first node of the list
+  * Return: the node where the loop starts, or NULL if there is no loop
+  **/
+listint_t	*find_listint_loop(listint_t *head)
+{
+	listint_t	*p_slow;
+	listint_t	*p_fast;
+
+	if (!head)
+		return (0);
+	p_slow = head;
+	p_fast = head;
+	while (1)
+	{
+		if (!p_fast || !p_fast->next)
+			return (NULL);
+		p_slow = p_slow->next;
+		p_fast = p_fast->next->next;
+		if (p_slow == p_fast)
+			break;
+	}
+	p_fast = head;
+	while (1)
+	{
+		p_slow = p_slow->next;
+		p_fast = p_fast->next;
+		if (p_slow == p_fast)
+			break;
+	}
+	return (p_slow);
+}
