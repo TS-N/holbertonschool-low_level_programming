44251f1c076e3d3e7406d06417e8511f7621b57c
TS-N <2501@holbertonschool.com>
2020-11-09 18:00:57 +0000

    fix:2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,44 @@
 #include "holberton.h"
 
 /**
-  * append_text_to_file - appends text at the end of a file
-  * @filename: the name of the file
-  * @text_content: the content to add to the file
-  * Return: 1 on success, -1 on failure
-  **/
+ * _strlen - gives the length of a string
+ * @s: the string from whitch len is found
+ *
+ * Return: the len of the string
+ **/
+size_t	_strlen(char *s)
+{
+	size_t	l = 0;
+
+	while (*s++)
+		++l;
+	return (l);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the content to add to the file
+ * Return: 1 on success, -1 on failure
+ **/
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int	fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	if (write(fd, text_content, _strlen(text_content)) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
 }
