69312b35bcfa6a3c574a432ff5d681b73a1eae0d
TS-N <2501@holbertonschool.com>
2021-01-15 10:13:32 +0000

    fix:4 more guards

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t	*hn;
 
+	if (!ht || !key || !*key)
+		return (NULL);
 	idx = key_index((const unsigned char *)key, ht->size);
 	hn = get_hn((hash_table_t *)ht, idx, key);
 	return (hn ? hn->value : NULL);
